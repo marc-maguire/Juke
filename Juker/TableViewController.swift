7fd18eedce1931c767398868d1c058c67192270c
Marc Maguire <maguire.marc@gmail.com>
2017-06-13 19:33:06 -0400

    song label section updated to include timer to track song duration

--- Juker/TableViewController.swift
+++ Juker/TableViewController.swift
@@ -16,12 +16,20 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var ArtistNameLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var albumArtImageView: UIImageView!
-    
+    var timer = Timer()
+    var counter = 0
+    var isTimerRunning = false
+    var resumeTapped = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        setTimer(seconds: 240)
+        durationLabel.text = String(counter)
+        startTimer()
+        
+        
 
         // Do any additional setup after loading the view.
     }
@@ -30,6 +38,41 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Timer Methods
+    
+    func setTimer(seconds: Int) {
+        counter = seconds
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(TableViewController.updateCounter)), userInfo: nil, repeats: true)
+        
+    }
+    func updateCounter() {
+        counter -= 1 //count up by 1 second at a time
+        durationLabel.text = timeString(time: TimeInterval(counter))
+    }
+    
+    func pauseTimer() {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            self.resumeTapped = true
+        } else {
+            startTimer()
+            self.resumeTapped = false
+        }
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+    
+        return String(format:"%02d:%02d", minutes, seconds)
+       
+    }
+    
+    
     
     //MARK: TableView Data Source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,7 +83,7 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! JukeTableViewCell
         
         return cell
-        
+         
     }
    
 }
