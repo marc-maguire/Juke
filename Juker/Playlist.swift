1e6eb00fc4074d8914d7552183610bcb5828eda6
Marc Maguire <maguire.marc@gmail.com>
2017-06-14 17:37:24 -0400

    finished merging changes

--- Juker/Playlist.swift
+++ Juker/Playlist.swift
@@ -1,1 +1,30 @@
-////  Playlist.swift//  Juker////  Created by Alex Mitchell on 2017-06-13.//  Copyright © 2017 Alex Mitchell. All rights reserved.//import UIKitclass Playlist: NSObject {        var name: String    var trackRequestURL: String    var playlistID: String    var ownerID: String    var image: String            init(jsonDictionary: [String:AnyObject]) {        let owner = jsonDictionary["owner"] as! [String : AnyObject]    let images = jsonDictionary["images"] as! [[String : AnyObject]]        self.name = jsonDictionary["name"] as! String    self.trackRequestURL = jsonDictionary["href"] as! String    self.playlistID = jsonDictionary["id"] as! String    self.ownerID = owner["id"] as! String    self.image = images.first?["url"] as! String    }}
+//
+//  Playlist.swift
+//  Juker
+//
+//  Created by Marc Maguire on 2017-06-14.
+//  Copyright Â© 2017 Alex Mitchell. All rights reserved.
+//
+
+import UIKit
+class Playlist: NSObject {
+    
+    var name: String
+    var trackRequestURL: String
+    var playlistID: String
+    var ownerID: String
+    var image: String
+    
+    
+    init(jsonDictionary: [String:AnyObject]) {
+        
+        let owner = jsonDictionary["owner"] as! [String : AnyObject]
+        let images = jsonDictionary["images"] as! [[String : AnyObject]]
+        
+        self.name = jsonDictionary["name"] as! String
+        self.trackRequestURL = jsonDictionary["href"] as! String
+        self.playlistID = jsonDictionary["id"] as! String
+        self.ownerID = owner["id"] as! String
+        self.image = images.first?["url"] as! String
+    }
+}
